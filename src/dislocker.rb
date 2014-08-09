df963869ab75d978f8844f1c3aae15ff989709e1
Aorimn <aorimn@gmail.com>
2014-08-09 13:28:46 +0200

    Test travis multi-OS builds

---
+++ src/dislocker.rb
@@ -0,0 +1,20 @@
+#
+# this brew file *must* be called 'dislocker.rb' to match the Formula
+#
+
+require "formula"
+
+class Dislocker < Formula
+  homepage "https://github.com/Aorimn/dislocker"
+  url "https://github.com/Aorimn/dislocker/archive/master.zip"
+  sha1 "f366f788d5a79c975e553f496ffd4eadc8427b2a"
+  version "0.3.1"
+
+  depends_on "polarssl"
+  depends_on "osxfuse"
+
+  def install
+    system "make fuse"
+    system "make install INSTALL_PATH=#{prefix}/ MAN_PATH=#{prefix}/"
+  end
+end

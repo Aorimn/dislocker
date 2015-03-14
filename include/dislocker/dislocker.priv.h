c9134665546870ad594dbf58644b672501d24725
Aorimn <aorimn@gmail.com>
2015-03-15 00:23:43 +0100

    Forgot some files in the 53ca6e4 commit
    It's well to put structures in private files, but it's better to add the
    files to the repo.

---
+++ include/dislocker/dislocker.priv.h
@@ -0,0 +1,69 @@
+/* -*- coding: utf-8 -*- */
+/* -*- mode: c -*- */
+/*
+ * Dislocker -- enables to read/write on BitLocker encrypted partitions under
+ * Linux
+ * Copyright (C) 2012-2013  Romain Coltel, Hervé Schauer Consultants
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+#ifndef DIS_DISLOCKER_PRIV_H
+#define DIS_DISLOCKER_PRIV_H
+
+#include <stdint.h>
+
+#include "dislocker/dislocker.h"
+#include "dislocker/config.priv.h"
+#include "dislocker/inouts/sectors.priv.h"
+
+
+
+#include "dislocker/return_values.h"
+#define checkupdate_dis_state(ctx, state)                       \
+	do {                                                        \
+		(ctx)->curr_state = (state);                            \
+		if((state) == (ctx)->cfg.init_stop_at) {                \
+			xprintf(L_DEBUG, "Exiting at state %d\n", (state)); \
+			return DIS_RET_SUCCESS;                             \
+		}                                                       \
+	} while(0);
+
+
+
+/**
+ * Main structure to pass to dislocker functions. These keeps various
+ * information in it.
+ */
+struct _dis_ctx {
+	/*
+	 * Dislocker's configuration.
+	 * Note that there's the dis_getopts() function to fill this structure from
+	 * command-line's arguments and the dis_setopt() function
+	 */
+	dis_config_t cfg;
+	
+	/*
+	 * Structure needed for dec/encryption processes.
+	 */
+	dis_iodata_t io_data;
+	
+	/*
+	 * States dislocker initialisation is at or will be stopped at.
+	 */
+	dis_state_e curr_state;
+};
+
+#endif /* DIS_DISLOCKER_PRIV_H */

ed6d4ec5a518125f2f7f4ea227b733954173efad
Dorian Muthig <contact@dorianmuthig.com>
2015-07-12 00:50:04 +0200

    fix home-brew script to make it actually work, that is, short of the strange ruby `File` class bug...

--- src/dislocker.rb
+++ src/dislocker.rb
@@ -5,20 +5,22 @@
 require "formula"
 
 class Dislocker < Formula
-  homepage "https://github.com/Aorimn/dislocker"
-  url "https://github.com/Aorimn/dislocker/archive/v0.4.tar.gz"
-  sha1 "d05858bd9d5d5fc0f21fef3ed6fa32bea13be762"
-  version "0.4.0"
-
-  depends_on "polarssl"
-  depends_on "osxfuse"
-
-  def install
-    # This is a bit hackish, but is there another way?
-    ssl_libname='polarssl'
-    ssl_libname='mbedtls' if File.exists? "#{lib}/libmbedtls.a"
-
-    system "make -C src SSLIB=#{ssl_libname}"
-    system "make -C src install DESTDIR=#{prefix}/"
-  end
+    homepage "https://github.com/Aorimn/dislocker"
+    url "https://github.com/Aorimn/dislocker/archive/cd9d8e2e760fb94826e073112421442fdd287776.zip"
+    sha256 "d319695711ae2bf0dbef67d686cf90081056ab1795671a4ea993d63035350d86"
+    version "0.4.2"
+    
+    depends_on "polarssl"
+    depends_on :osxfuse
+    
+    def install
+        # This is a bit hackish, but is there another way?
+        ssl_libname='mbedtls'
+        if File.exists?("#{lib}/polarssl.a") && !File.exists?("/usr/local/Cellar/polarssl/1.3.10/mbedtls.a") # for some reason this returns false on my machine... I was fed up with it so I swapped the legacy support crap
+            ssl_libname='polarssl'
+        end
+        
+        system "make -C src SSLIB=#{ssl_libname}"
+        system "make -C src install DESTDIR=#{prefix}/"
+    end
 end

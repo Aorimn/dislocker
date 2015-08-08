b9ffd7318e2b31c6da95b2c3b44ac70ee5562b57
Aorimn <aorimn@gmail.com>
2015-08-08 17:16:17 +0200

    Fix running issues on OSX
    The library didn't expose properly its version/compatibility version so
    binaries would compile finely with the library but not execute.
    Also, the mbedtls 2.0.0 issue is now scripted to be solved, it's even
    better than to do it by hand.

---
+++ src/mbed_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ `brew info polarssl |head -1 |grep 2.0.0` ]
+then
+	git clone https://github.com/ARMmbed/mbedtls.git
+	cd mbedtls
+	git checkout mbedtls-2.0.0
+	git cherry-pick -n --strategy=recursive -Xours 6f42417ba8dd28fa77fd08d42d73c87a0253f93e
+	cmake .
+	make VERBOSE=1
+	make install
+	cd ..
+else
+	brew install -v polarssl
+fi

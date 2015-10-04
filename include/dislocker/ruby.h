45dce08b0e78712b9cbec57ebc6ea1c4523a7a9c
Aorimn <aorimn@gmail.com>
2015-10-04 16:16:22 +0200

    Improve compiler compatibility again
    Make clang statements only for clang.

--- include/dislocker/ruby.h
+++ include/dislocker/ruby.h
@@ -25,12 +25,18 @@
 
 #ifdef _HAVE_RUBY
 
+#if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunknown-pragmas"
 #pragma clang diagnostic ignored "-Wunknown-attributes"
-#include <ruby.h>
-#pragma clang diagnostic pop
+#endif /* defined(__clang__) */
 
-#endif
+#include <ruby.h>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif /* defined(__clang__) */
+
+#endif /* _HAVE_RUBY */
 
 #endif /* DIS_RUBY_H */

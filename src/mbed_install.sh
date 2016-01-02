0ad40a5d20b91bec469267c11ac1d538c0487c59
Aorimn <aorimn@gmail.com>
2016-01-02 13:21:34 +0100

    Clone mbedtls in an explicitly named directory
    In the mbed_install.sh script, we clone the mbedtls's github repository
    in an explicitly named folder.

--- src/mbed_install.sh
+++ src/mbed_install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+MBEDTLS_FOLDER=mbedtls-for-dislocker
+
 if brew info polarssl |head -1 |grep -q 2.0.0
 then
-	git clone https://github.com/ARMmbed/mbedtls.git
-	cd mbedtls
+	git clone https://github.com/ARMmbed/mbedtls.git ${MBEDTLS_FOLDER}
+	cd ${MBEDTLS_FOLDER}
 	git checkout mbedtls-2.0.0
 	git cherry-pick -n --strategy=recursive -Xours 6f42417ba8dd28fa77fd08d42d73c87a0253f93e
 	cmake .
 	make VERBOSE=1
 	make install
 	cd ..
-	rm -fr mbedtls
+	rm -fr ${MBEDTLS_FOLDER}
 else
 	brew install -v polarssl
 fi

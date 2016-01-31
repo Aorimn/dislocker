0f14ed5baafd624b65f5b79b3997d30415901d64
Aorimn <aorimn@gmail.com>
2016-01-31 16:42:11 +0100

    Change crc32 to our own implementation
    Although we use the table from the previous implementation, this enables
    us to use our own license.

--- include/dislocker/encryption/crc32.h
+++ include/dislocker/encryption/crc32.h
@@ -1,36 +1,31 @@
+/* -*- coding: utf-8 -*- */
+/* -*- mode: c -*- */
 /*
-
-crc32.h
-
-Author: Tatu Ylonen <ylo@cs.hut.fi>
-
-Copyright (c) 1992 Tatu Ylonen, Espoo, Finland
-                   All rights reserved
-
-Created: Tue Feb 11 14:37:27 1992 ylo
-
-Functions for computing 32-bit CRC.
-
-*/
-
-/*
- * $Id: crc32.h,v 1.1.1.1 1996/02/18 21:38:11 ylo Exp $
- * $Log: crc32.h,v $
- * Revision 1.1.1.1  1996/02/18 21:38:11  ylo
- * 	Imported ssh-1.2.13.
+ * Dislocker -- enables to read/write on BitLocker encrypted partitions under
+ * Linux
+ * Copyright (C) 2012-2013  Romain Coltel, Hervé Schauer Consultants
  *
- * Revision 1.2  1995/07/13  01:21:45  ylo
- * 	Removed "Last modified" header.
- * 	Added cvs log.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * $Endlog$
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
-
 #ifndef CRC32_H
 #define CRC32_H
 
-/* This computes a 32 bit CRC of the data in the buffer, and returns the
-   CRC.  The polynomial used is 0xedb88320. */
-unsigned int crc32(const unsigned char *buf, unsigned int len);
+/*
+ * Prototypes
+ */
+unsigned int crc32(const unsigned char *buf, const unsigned int len);
 
 #endif /* CRC32_H */
